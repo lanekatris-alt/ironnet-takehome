2894b64902ca15b4b557054786cdb01dd61c2e88
Lane Katris <lanekatris@gmail.com>
2021-05-28 08:58:23 -0400

    initial project and working on resolvers and schema

---
+++ index.js
@@ -0,0 +1,50 @@
+const { ApolloServer, gql } = require("apollo-server");
+const rawLaunches = require("./spacex_launches.json");
+
+const launches = rawLaunches.map(launch => ({
+  name: launch.mission_name
+}))
+
+// This is the definition of the GraphQL schema
+// How you structure this schema to fit the spacex_launches_data below
+// along with how you write the code is the primary assessment
+// Make input required?
+const typeDefs = gql`
+    
+    type Query {
+        launches(input: MissionsInput): [Launch]
+    }
+    
+    input MissionsInput {
+        name: String
+    }
+    
+    type Launch {
+        name: String!
+    }
+`;
+
+// Resolvers define the technique for fetching the types defined in the schema.
+// Launch: {
+//   name: (launch) => launch.mission_name
+// }
+const resolvers = {
+  Query: {
+    launches(parent, {input}, context, info) {
+      const { name } = input || {};
+      console.log({name})
+
+      return launches;
+    }
+  }
+
+
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+})
+
